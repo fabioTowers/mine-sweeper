f33f3ed51b2a352a7a3ef14d74095ca9d9dd91c0
Fabio Mendes <fabiomendes53@gmail.com>
2021-12-28 14:44:56 -0300

    feat: create parameters file

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react/cjs/react.production.min';
+import params from './src/params';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default  class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mine Sweeper!</Text>
+        <Text>Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +20,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
 });
